acbcdad58350da23d8a24ce387b03d3a6133a96e
Patrice <patricelamarre@yahoo.fr>
2024-12-13 11:45:53 -0500

    actua5

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -7,6 +7,12 @@ const dbConnection = new Sequelize(process.env.MYSQL_URL, {
     logging: false,
 });
 
+dbConnection
+  .authenticate()
+  .then(() => console.log('Conexión a la base de datos exitosa.'))
+  .catch((err) => console.error('Error al conectar a la base de datos:', err));
+
+
 //MYSQL_URL=mysql://root:jcdGeyqiuAimYQxwmPcejVCiwcxQUuai@mysql.railway.internal:3306/railway
 
 
